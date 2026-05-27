71644fdb85fb3e7d98f87b53625c41dd25417627
harinirajendran26 <264harinirajendran@gmail.com>
2026-05-27 22:06:47 +0530

    Fixed Neon DB connection

--- database.py
+++ database.py
@@ -19,6 +19,7 @@ def get_connection():
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD")
+        sslmode="require"
     )
 
 def run_query(sql: str):
