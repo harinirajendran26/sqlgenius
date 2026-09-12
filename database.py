d7e153663feea8e8f73abfb2c2543bf7899f771d
harinirajendran26 <264harinirajendran@gmail.com>
2026-09-12 18:52:31 +0530

    fix: connect to Neon PostgreSQL with SSL

--- database.py
+++ database.py
@@ -14,13 +14,13 @@ def get_connection():
     We call this every time we need to run a query.
     """
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        sslmode="require"
-    )
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    sslmode=os.getenv("DB_SSLMODE","require")
+)
 
 def run_query(sql: str):
     """
